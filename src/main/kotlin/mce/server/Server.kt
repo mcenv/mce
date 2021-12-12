27e5dc98422f80ebd8b7cf0df8a704d98351be0a
intsuc <intsuc@gmail.com>
2021-12-12 13:54:31 +0900

    Track dependencies

--- src/main/kotlin/mce/server/Server.kt
+++ src/main/kotlin/mce/server/Server.kt
@@ -1,12 +1,40 @@
 package mce.server
 
+import mce.phase.Elaborate
 import java.util.*
-import kotlin.system.exitProcess
+import mce.graph.Core as C
+import mce.graph.Surface as S
 
 class Server {
-    suspend fun build(): Unit = TODO()
+    private val dependencies: MutableMap<String, MutableList<String>> = mutableMapOf()
+    private val surfaces: MutableMap<String, S.Item> = mutableMapOf()
+    private val cores: MutableMap<String, C.Item> = mutableMapOf()
 
-    suspend fun hover(id: UUID): HoverItem = TODO()
+    fun register(surface: S.Item) {
+        surface.imports.forEach {
+            dependencies.computeIfAbsent(it) { mutableListOf() } += surface.name
+        }
+        surfaces[surface.name] = surface
+    }
 
-    fun exit(): Nothing = exitProcess(0)
+    suspend fun fetch(name: String): C.Item = cores.getOrElse(name) {
+        val (core, diagnostics) = Elaborate().run(surfaces[name]!!)
+        core
+    }
+
+    fun edit(name: String) {
+        dependencies[name]!!.forEach {
+            cores -= it
+        }
+    }
+
+    suspend fun hover(name: String, id: UUID): HoverItem = TODO()
+
+    suspend fun build() {
+        surfaces.keys.forEach {
+            fetch(it)
+        }
+    }
+
+    fun exit(): Nothing = TODO()
 }
