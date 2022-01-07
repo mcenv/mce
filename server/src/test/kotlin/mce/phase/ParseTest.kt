647ec6234dca9e2e42098b71c00c0ef5309bf2df
intsuc <intsuc@gmail.com>
2022-01-07 14:22:46 +0900

    Simplify type names

--- server/src/test/kotlin/mce/phase/ParseTest.kt
+++ server/src/test/kotlin/mce/phase/ParseTest.kt
@@ -7,11 +7,10 @@ import java.util.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
 
-@Suppress("RECEIVER_NULLABILITY_MISMATCH_BASED_ON_JAVA_ANNOTATIONS")
 class ParseTest {
     @Test
     fun testFalse() {
-        ParseTest::class.java.getResourceAsStream("/false.mce").use {
+        ParseTest::class.java.getResourceAsStream("/false.mce")!!.use {
             assertEquals(
                 Item.Definition(
                     UUID(0, 1),
