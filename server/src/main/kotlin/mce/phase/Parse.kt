1b3a2b5d6e13d09775babfdebbc6667f4f924e84
intsuc <intsuc@gmail.com>
2022-02-08 17:13:20 +0900

    Use textual representation for tests

--- server/src/main/kotlin/mce/phase/Parse.kt
+++ server/src/main/kotlin/mce/phase/Parse.kt
@@ -1,5 +1,6 @@
 package mce.phase
 
+import mce.graph.Id
 import mce.graph.freshId
 import java.util.*
 import mce.graph.Surface as S
@@ -9,109 +10,113 @@ class Parse private constructor(
 ) {
     private var cursor: Int = 0
 
-    private fun parseItem(): S.Item {
+    private fun parseItem(id: Id = freshId()): S.Item {
         val imports = parseParen {
-            if (readWord() != "import") error()
+            if (readWord() != "import") error("'import' expected")
             parseList(::readWord)
         }
 
         return parseParen {
-            when (readWord()) {
-                "definition" -> S.Item.Definition(imports, parseList(::parseModifier), readWord(), parseTerm(), parseTerm(), freshId())
-                else -> error()
+            when (val word = readWord()) {
+                "definition" -> S.Item.Definition(imports, parseList(::parseModifier), readWord(), parseTerm(), parseTerm(), it)
+                else -> error("unexpected item '$word'")
             }
         }
     }
 
-    private fun parseModifier(): S.Modifier = when (readWord()) {
+    private fun parseModifier(): S.Modifier = when (val word = readWord()) {
         "meta" -> S.Modifier.META
-        else -> error()
+        else -> error("unexpected modifier '$word'")
     }
 
-    private fun parseTerm(id: () -> UUID = ::freshId): S.Term = when (run { skipWhitespace(); peek() }) {
-        '(' -> parseParen { parseTerm() }
-        '"' -> S.Term.StringOf(readString(), id())
-        '#' -> {
-            skip()
-            val explicitId = UUID.fromString(readWord())
-            parseTerm { explicitId }
-        }
+    private fun parseTerm(id: Id = freshId()): S.Term = when (run { skipWhitespace(); peek() }) {
+        '(' -> parseParen { parseTerm(it) }
+        '"' -> S.Term.StringOf(readString(), id)
         else -> when (val word = readWord()) {
-            "hole" -> S.Term.Hole(id())
-            "meta" -> S.Term.Meta(readWord().toInt(), id())
-            "definition" -> S.Term.Definition(readWord(), id())
-            "let" -> S.Term.Let(readWord(), parseTerm(), parseTerm(), id())
-            "match" -> S.Term.Match(parseTerm(), parseList { parsePair(::parsePattern, ::parseTerm) }, id())
-            "false" -> S.Term.BooleanOf(false, id())
-            "true" -> S.Term.BooleanOf(true, id())
-            "byte_array_of" -> S.Term.ByteArrayOf(parseList { parseTerm() }, id())
-            "int_array_of" -> S.Term.IntArrayOf(parseList { parseTerm() }, id())
-            "long_array_of" -> S.Term.LongArrayOf(parseList { parseTerm() }, id())
-            "list_of" -> S.Term.ListOf(parseList { parseTerm() }, id())
-            "compound_of" -> S.Term.CompoundOf(parseList { parseTerm() }, id())
-            "reference_of" -> S.Term.ReferenceOf(parseTerm(), id())
-            "function_of" -> S.Term.FunctionOf(parseList(::readWord), parseTerm(), id())
-            "apply" -> S.Term.Apply(parseTerm(), parseList { parseTerm() }, id())
-            "code_of" -> S.Term.CodeOf(parseTerm(), id())
-            "splice" -> S.Term.Splice(parseTerm(), id())
-            "union" -> S.Term.Union(parseList { parseTerm() }, id())
-            "intersection" -> S.Term.Intersection(parseList { parseTerm() }, id())
-            "boolean" -> S.Term.Boolean(id())
-            "byte" -> S.Term.Byte(id())
-            "short" -> S.Term.Short(id())
-            "int" -> S.Term.Int(id())
-            "long" -> S.Term.Long(id())
-            "float" -> S.Term.Float(id())
-            "double" -> S.Term.Double(id())
-            "string" -> S.Term.String(id())
-            "byte_array" -> S.Term.ByteArray(id())
-            "int_array" -> S.Term.IntArray(id())
-            "long_array" -> S.Term.LongArray(id())
-            "list" -> S.Term.List(parseTerm(), id())
-            "compound" -> S.Term.Compound(parseList { parsePair(::readWord, ::parseTerm) }, id())
-            "reference" -> S.Term.Reference(parseTerm(), id())
-            "function" -> S.Term.Function(parseList { parseParen { S.Parameter(readWord(), parseTerm(), parseTerm(), parseTerm()) } }, parseTerm(), id())
-            "code" -> S.Term.Code(parseTerm(), id())
-            "type" -> S.Term.Type(id())
+            "hole" -> S.Term.Hole(id)
+            "meta" -> S.Term.Meta(readWord().toInt(), id)
+            "definition" -> S.Term.Definition(readWord(), id)
+            "let" -> S.Term.Let(readWord(), parseTerm(), parseTerm(), id)
+            "match" -> S.Term.Match(parseTerm(), parseList { parsePair(::parsePattern, ::parseTerm) }, id)
+            "false" -> S.Term.BooleanOf(false, id)
+            "true" -> S.Term.BooleanOf(true, id)
+            "byte_array_of" -> S.Term.ByteArrayOf(parseList { parseTerm() }, id)
+            "int_array_of" -> S.Term.IntArrayOf(parseList { parseTerm() }, id)
+            "long_array_of" -> S.Term.LongArrayOf(parseList { parseTerm() }, id)
+            "list_of" -> S.Term.ListOf(parseList { parseTerm() }, id)
+            "compound_of" -> S.Term.CompoundOf(parseList { parseTerm() }, id)
+            "reference_of" -> S.Term.ReferenceOf(parseTerm(), id)
+            "function_of" -> S.Term.FunctionOf(parseList(::readWord), parseTerm(), id)
+            "apply" -> S.Term.Apply(parseTerm(), parseList { parseTerm() }, id)
+            "code_of" -> S.Term.CodeOf(parseTerm(), id)
+            "splice" -> S.Term.Splice(parseTerm(), id)
+            "union" -> S.Term.Union(parseList { parseTerm() }, id)
+            "end" -> S.Term.Union(emptyList(), id)
+            "intersection" -> S.Term.Intersection(parseList { parseTerm() }, id)
+            "any" -> S.Term.Intersection(emptyList(), id)
+            "boolean" -> S.Term.Boolean(id)
+            "byte" -> S.Term.Byte(id)
+            "short" -> S.Term.Short(id)
+            "int" -> S.Term.Int(id)
+            "long" -> S.Term.Long(id)
+            "float" -> S.Term.Float(id)
+            "double" -> S.Term.Double(id)
+            "string" -> S.Term.String(id)
+            "byte_array" -> S.Term.ByteArray(id)
+            "int_array" -> S.Term.IntArray(id)
+            "long_array" -> S.Term.LongArray(id)
+            "list" -> S.Term.List(parseTerm(), id)
+            "compound" -> S.Term.Compound(parseList { parsePair(::readWord, ::parseTerm) }, id)
+            "reference" -> S.Term.Reference(parseTerm(), id)
+            "function" -> S.Term.Function(parseList { parseParen { S.Parameter(readWord(), parseTerm(), parseTerm(), parseTerm()) } }, parseTerm(), id)
+            "code" -> S.Term.Code(parseTerm(), id)
+            "type" -> S.Term.Type(id)
             else -> when {
-                BYTE_EXPRESSION.matches(word) -> S.Term.ByteOf(word.dropLast(1).toByte(), id())
-                SHORT_EXPRESSION.matches(word) -> S.Term.ShortOf(word.dropLast(1).toShort(), id())
-                INT_EXPRESSION.matches(word) -> S.Term.IntOf(word.toInt(), id())
-                LONG_EXPRESSION.matches(word) -> S.Term.LongOf(word.dropLast(1).toLong(), id())
-                FLOAT_EXPRESSION.matches(word) -> S.Term.FloatOf(word.dropLast(1).toFloat(), id())
-                DOUBLE_EXPRESSION.matches(word) -> S.Term.DoubleOf(word.dropLast(1).toDouble(), id())
-                else -> S.Term.Variable(word, id())
+                BYTE_EXPRESSION.matches(word) -> S.Term.ByteOf(word.dropLast(1).toByte(), id)
+                SHORT_EXPRESSION.matches(word) -> S.Term.ShortOf(word.dropLast(1).toShort(), id)
+                INT_EXPRESSION.matches(word) -> S.Term.IntOf(word.toInt(), id)
+                LONG_EXPRESSION.matches(word) -> S.Term.LongOf(word.dropLast(1).toLong(), id)
+                FLOAT_EXPRESSION.matches(word) -> S.Term.FloatOf(word.dropLast(1).toFloat(), id)
+                DOUBLE_EXPRESSION.matches(word) -> S.Term.DoubleOf(word.dropLast(1).toDouble(), id)
+                else -> S.Term.Variable(word, id)
             }
         }
     }
 
-    private fun parsePattern(): S.Pattern = when (run { skipWhitespace(); peek() }) {
-        '(' -> parseParen(::parsePattern)
-        '"' -> S.Pattern.StringOf(readString(), freshId())
+    private fun parsePattern(id: Id = freshId()): S.Pattern = when (run { skipWhitespace(); peek() }) {
+        '(' -> parseParen { parsePattern(it) }
+        '"' -> S.Pattern.StringOf(readString(), id)
         else -> when (val word = readWord()) {
-            "false" -> S.Pattern.BooleanOf(false, freshId())
-            "true" -> S.Pattern.BooleanOf(true, freshId())
-            "byte_array_of" -> S.Pattern.ByteArrayOf(parseList(::parsePattern), freshId())
-            "int_array_of" -> S.Pattern.IntArrayOf(parseList(::parsePattern), freshId())
-            "long_array_of" -> S.Pattern.LongArrayOf(parseList(::parsePattern), freshId())
-            "list_of" -> S.Pattern.ListOf(parseList(::parsePattern), freshId())
-            "compound_of" -> S.Pattern.CompoundOf(parseList(::parsePattern), freshId())
-            "reference_of" -> S.Pattern.ReferenceOf(parsePattern(), freshId())
+            "false" -> S.Pattern.BooleanOf(false, id)
+            "true" -> S.Pattern.BooleanOf(true, id)
+            "byte_array_of" -> S.Pattern.ByteArrayOf(parseList(::parsePattern), id)
+            "int_array_of" -> S.Pattern.IntArrayOf(parseList(::parsePattern), id)
+            "long_array_of" -> S.Pattern.LongArrayOf(parseList(::parsePattern), id)
+            "list_of" -> S.Pattern.ListOf(parseList(::parsePattern), id)
+            "compound_of" -> S.Pattern.CompoundOf(parseList(::parsePattern), id)
+            "reference_of" -> S.Pattern.ReferenceOf(parsePattern(), id)
             else -> when {
-                BYTE_EXPRESSION.matches(word) -> S.Pattern.ByteOf(word.dropLast(1).toByte(), freshId())
-                SHORT_EXPRESSION.matches(word) -> S.Pattern.ShortOf(word.dropLast(1).toShort(), freshId())
-                INT_EXPRESSION.matches(word) -> S.Pattern.IntOf(word.toInt(), freshId())
-                LONG_EXPRESSION.matches(word) -> S.Pattern.LongOf(word.dropLast(1).toLong(), freshId())
-                FLOAT_EXPRESSION.matches(word) -> S.Pattern.FloatOf(word.dropLast(1).toFloat(), freshId())
-                DOUBLE_EXPRESSION.matches(word) -> S.Pattern.DoubleOf(word.dropLast(1).toDouble(), freshId())
-                else -> S.Pattern.Variable(word, freshId())
+                BYTE_EXPRESSION.matches(word) -> S.Pattern.ByteOf(word.dropLast(1).toByte(), id)
+                SHORT_EXPRESSION.matches(word) -> S.Pattern.ShortOf(word.dropLast(1).toShort(), id)
+                INT_EXPRESSION.matches(word) -> S.Pattern.IntOf(word.toInt(), id)
+                LONG_EXPRESSION.matches(word) -> S.Pattern.LongOf(word.dropLast(1).toLong(), id)
+                FLOAT_EXPRESSION.matches(word) -> S.Pattern.FloatOf(word.dropLast(1).toFloat(), id)
+                DOUBLE_EXPRESSION.matches(word) -> S.Pattern.DoubleOf(word.dropLast(1).toDouble(), id)
+                else -> S.Pattern.Variable(word, id)
             }
         }
     }
 
-    private inline fun <A> parseParen(parseA: () -> A): A {
+    private inline fun <A> parseParen(parseA: (Id) -> A): A {
         expect('(')
-        return parseA().also { expect(')') }
+        return when (run { skipWhitespace(); peek() }) {
+            '#' -> {
+                skip()
+                val explicitId = UUID.fromString(readWord())
+                parseA(explicitId)
+            }
+            else -> parseA(freshId())
+        }.also { expect(')') }
     }
 
     private inline fun <A, B> parsePair(parseA: () -> A, parseB: () -> B): Pair<A, B> = parseParen { parseA() to parseB() }
@@ -136,7 +141,7 @@ class Parse private constructor(
 
     private fun expect(char: Char) {
         skipWhitespace()
-        if (!canRead() || peek() != char) error()
+        if (!canRead() || peek() != char) error("'$char' expected")
         skip()
     }
 
@@ -144,7 +149,7 @@ class Parse private constructor(
         skipWhitespace()
         val start = cursor
         while (canRead() && peek().let { it != ' ' && it != '(' && it != ')' }) skip()
-        return source.substring(start, cursor).also { if (it.isEmpty()) error() }
+        return source.substring(start, cursor).also { if (it.isEmpty()) error("word expected") }
     }
 
     // TODO: handle escape
@@ -155,7 +160,7 @@ class Parse private constructor(
         return source.substring(start, cursor).also { skip() }
     }
 
-    private fun error(): Nothing = throw Error("$cursor")
+    private fun error(message: String): Nothing = throw Error("$message at $cursor")
 
     companion object {
         private val BYTE_EXPRESSION = Regex("[-+]?(?:0|[1-9][0-9]*)b")
--- server/src/test/kotlin/mce/phase/ElaborateTest.kt
+++ server/src/test/kotlin/mce/phase/ElaborateTest.kt
@@ -1,29 +1,6 @@
 package mce.phase
 
 import mce.Diagnostic
-import mce.graph.Dsl.any
-import mce.graph.Dsl.boolean
-import mce.graph.Dsl.code
-import mce.graph.Dsl.code_of
-import mce.graph.Dsl.compound
-import mce.graph.Dsl.compound_of
-import mce.graph.Dsl.definition
-import mce.graph.Dsl.end
-import mce.graph.Dsl.ff
-import mce.graph.Dsl.function
-import mce.graph.Dsl.function_of
-import mce.graph.Dsl.invoke
-import mce.graph.Dsl.let_in
-import mce.graph.Dsl.match
-import mce.graph.Dsl.not
-import mce.graph.Dsl.p_ff
-import mce.graph.Dsl.p_tt
-import mce.graph.Dsl.p_variable
-import mce.graph.Dsl.parameter
-import mce.graph.Dsl.tt
-import mce.graph.Dsl.type
-import mce.graph.Dsl.variable
-import mce.graph.Surface.Modifier.META
 import mce.read
 import java.util.*
 import kotlin.test.Test
@@ -48,255 +25,88 @@ class ElaborateTest {
     }
 
     @Test
-    fun testApply() {
-        val (_, diagnostics, _) = Elaborate(
-            emptyMap(),
-            definition(
-                emptyList(),
-                "a",
-                boolean(),
-                function_of(variable("x"), "x")(ff())
-            )
-        )
-
-        assert(diagnostics.isEmpty())
-    }
-    @Test
-    fun testDependentFunctionIntroduction() {
-        val (_, diagnostics, _) = Elaborate(
-            emptyMap(),
-            definition(
-                emptyList(),
-                "a",
-                function(variable("x"), parameter("x", end(), any(), type()), parameter("y", end(), any(), variable("x"))),
-                function_of(variable("y"), "x", "y")
-            )
-        )
-
+    fun apply() {
+        val (_, diagnostics, _) = elaborate(emptyMap(), "/apply.mce")
         assert(diagnostics.isEmpty())
     }
 
     @Test
-    fun testDependentFunctionElimination() {
-        val (identity, diagnostics1, _) = Elaborate(
-            emptyMap(),
-            definition(
-                emptyList(),
-                "identity",
-                function(variable("α"), parameter("α", end(), any(), type()), parameter("a", end(), any(), variable("α"))),
-                function_of(variable("a"), "α", "a")
-            )
-        )
-        val (_, diagnostics2, _) = Elaborate(
-            mapOf(identity.name to identity),
-            definition(
-                emptyList(),
-                "f",
-                boolean(),
-                definition("identity")(boolean(), ff())
-            )
-        )
+    fun functionIntro() {
+        val (_, diagnostics, _) = elaborate(emptyMap(), "/function_intro.mce")
+        assert(diagnostics.isEmpty())
+    }
 
+    @Test
+    fun functionElim() {
+        val (identity, diagnostics1, _) = elaborate(emptyMap(), "/identity.mce")
+        val (_, diagnostics2, _) = elaborate(mapOf(identity.name to identity), "/function_elim.mce")
         assert(diagnostics1.isEmpty())
         assert(diagnostics2.isEmpty())
     }
 
     @Test
     fun testFunctionClosed() {
-        val a = variable("a")
-        val (_, diagnostics, _) = Elaborate(
-            emptyMap(),
-            definition(
-                emptyList(),
-                "a",
-                function(function(boolean(), parameter("b", end(), any(), boolean())), parameter("a", end(), any(), boolean())),
-                function_of(function_of(a, "b"), "a")
-            )
-        )
-
-        assert(diagnostics.contains(Diagnostic.VariableNotFound("a", a.id)))
+        val (_, diagnostics, _) = elaborate(emptyMap(), "/function_closed.mce")
+        assert(diagnostics.contains(Diagnostic.VariableNotFound("a", UUID(0, 0))))
     }
 
     @Test
     fun testLet() {
-        val (_, diagnostics, _) = Elaborate(
-            emptyMap(),
-            definition(
-                emptyList(),
-                "a",
-                boolean(),
-                let_in("x", ff(), variable("x"))
-            )
-        )
-
-        assert(diagnostics.isEmpty())
-    }
-
-    @Test
-    fun testValidDefinition() {
-        val (a, _, _) = Elaborate(
-            emptyMap(),
-            definition(
-                emptyList(),
-                "a",
-                boolean(),
-                ff()
-            )
-        )
-        val (_, diagnostics, _) = Elaborate(
-            mapOf(a.name to a),
-            definition(
-                emptyList(),
-                "b",
-                boolean(),
-                definition("a")
-            )
-        )
-
+        val (_, diagnostics, _) = elaborate(emptyMap(), "/let.mce")
         assert(diagnostics.isEmpty())
     }
 
     @Test
     fun testInvalidDefinition() {
-        val a = definition("a")
-        val (_, diagnostics, _) = Elaborate(
-            emptyMap(),
-            definition(
-                emptyList(),
-                "b",
-                boolean(),
-                a
-            )
-        )
-
-        assert(diagnostics.contains(Diagnostic.DefinitionNotFound("a", a.id)))
+        val (_, diagnostics, _) = elaborate(emptyMap(), "/invalid_definition.mce")
+        assert(diagnostics.contains(Diagnostic.DefinitionNotFound("a", UUID(0, 0))))
     }
 
     @Test
     fun testDependentCompound() {
-        val (_, diagnostics, _) = Elaborate(
-            emptyMap(),
-            definition(
-                emptyList(),
-                "a",
-                compound("α" to type(), "a" to variable("α")),
-                compound_of(boolean(), ff())
-            )
-        )
-
+        val (_, diagnostics, _) = elaborate(emptyMap(), "/compound.mce")
         assert(diagnostics.isEmpty())
     }
 
     @Test
     fun testCodeIntroduction() {
-        val (_, diagnostics, _) = Elaborate(
-            emptyMap(),
-            definition(
-                listOf(META),
-                "a",
-                code(boolean()),
-                code_of(ff())
-            )
-        )
-
+        val (_, diagnostics, _) = elaborate(emptyMap(), "/code_intro.mce")
         assert(diagnostics.isEmpty())
     }
 
     @Test
     fun testCodeElimination() {
-        val (_, diagnostics, _) = Elaborate(
-            emptyMap(),
-            definition(
-                emptyList(),
-                "a",
-                boolean(),
-                !code_of(ff())
-            )
-        )
-
+        val (_, diagnostics, _) = elaborate(emptyMap(), "/code_elim.mce")
         assert(diagnostics.isEmpty())
     }
 
     @Test
     fun testStageMismatch() {
-        val b = variable("b")
-        val (_, diagnostics, _) = Elaborate(
-            emptyMap(),
-            definition(
-                listOf(META),
-                "a",
-                code(boolean()),
-                let_in("b", ff(), code_of(b))
-            )
-        )
-
-        assert(diagnostics.contains(Diagnostic.StageMismatch(1, 0, b.id)))
+        val (_, diagnostics, _) = elaborate(emptyMap(), "/stage_mismatch.mce")
+        assert(diagnostics.contains(Diagnostic.StageMismatch(1, 0, UUID(0, 0))))
     }
 
     @Test
     fun testPhaseMismatch() {
-        val d = definition(
-            emptyList(),
-            "a",
-            code(boolean()),
-            code_of(ff())
-        )
-        val (_, diagnostics, _) = Elaborate(
-            emptyMap(),
-            d
-        )
-
-        assert(diagnostics.contains(Diagnostic.PhaseMismatch(d.id)))
+        val (_, diagnostics, _) = elaborate(emptyMap(), "/phase_mismatch.mce")
+        assert(diagnostics.contains(Diagnostic.PhaseMismatch(UUID(0, 0))))
     }
 
     @Test
     fun testPhaseMismatchLocal() {
-        val l = let_in("b", code_of(ff()), ff())
-        val (_, diagnostics, _) = Elaborate(
-            emptyMap(),
-            definition(
-                emptyList(),
-                "a",
-                boolean(),
-                l
-            )
-        )
-
-        assert(diagnostics.contains(Diagnostic.PhaseMismatch(l.id)))
+        val (_, diagnostics, _) = elaborate(emptyMap(), "/phase_mismatch_local.mce")
+        assert(diagnostics.contains(Diagnostic.PhaseMismatch(UUID(0, 0))))
     }
 
     @Test
     fun testMatchBoolean() {
-        val (_, diagnostics, _) = Elaborate(
-            emptyMap(),
-            definition(
-                emptyList(),
-                "not",
-                boolean(),
-                ff().match(
-                    p_ff() to tt(),
-                    p_tt() to ff()
-                )
-            )
-        )
-
+        val (_, diagnostics, _) = elaborate(emptyMap(), "/match_boolean.mce")
         assert(diagnostics.isEmpty())
     }
 
     @Test
     fun testMatchVariable() {
-        val (_, diagnostics, _) = Elaborate(
-            emptyMap(),
-            definition(
-                emptyList(),
-                "a",
-                boolean(),
-                ff().match(
-                    p_variable("b") to variable("b")
-                )
-            )
-        )
-
+        val (_, diagnostics, _) = elaborate(emptyMap(), "/match_variable.mce")
         assert(diagnostics.isEmpty())
     }
 }
