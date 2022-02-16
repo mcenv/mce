392fb1b5f993e84cc919239f95aeb83b4c990464
intsuc <intsuc@gmail.com>
2022-02-16 19:57:06 +0900

    Add builtins

--- server/src/main/kotlin/mce/Builtins.kt
+++ server/src/main/kotlin/mce/Builtins.kt
@@ -7,5 +7,25 @@ val BUILTINS: Map<String, (List<Lazy<C.Value>>) -> C.Value> = mapOf(
         val a = arguments[0].value as C.Value.IntOf
         val b = arguments[1].value as C.Value.IntOf
         C.Value.IntOf(a.value + b.value)
+    },
+    "int/sub" to { arguments ->
+        val a = arguments[0].value as C.Value.IntOf
+        val b = arguments[1].value as C.Value.IntOf
+        C.Value.IntOf(a.value - b.value)
+    },
+    "int/mul" to { arguments ->
+        val a = arguments[0].value as C.Value.IntOf
+        val b = arguments[1].value as C.Value.IntOf
+        C.Value.IntOf(a.value * b.value)
+    },
+    "int/div" to { arguments ->
+        val a = arguments[0].value as C.Value.IntOf
+        val b = arguments[1].value as C.Value.IntOf
+        C.Value.IntOf(Math.floorDiv(a.value, b.value))
+    },
+    "int/mod" to { arguments ->
+        val a = arguments[0].value as C.Value.IntOf
+        val b = arguments[1].value as C.Value.IntOf
+        C.Value.IntOf(Math.floorMod(a.value, b.value))
     }
 )
