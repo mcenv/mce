ad27853768612a7d8d11db726c6dc856194a9456
intsuc <intsuc@gmail.com>
2022-02-25 19:54:06 +0900

    Remove client subproject

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,1 @@
 rootProject.name = "mce"
-include("client")
-include("server")
-
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-    }
-}
