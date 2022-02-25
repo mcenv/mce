d581c4dcc6a5b0b7b3c3ccdfba9e1aea1b013963
intsuc <intsuc@gmail.com>
2022-02-25 12:36:24 +0900

    Add array length builtins

--- server/src/main/kotlin/mce/Builtins.kt
+++ server/src/main/kotlin/mce/Builtins.kt
@@ -23,7 +23,7 @@ val BUILTINS: Map<String, Normalizer.() -> C.Value> = mapOf(
             a is C.Value.IntOf && a.value == 0 -> b
             // a + 0 = a
             b is C.Value.IntOf && b.value == 0 -> a
-            else -> C.Value.Def("int/add", mutableListOf(a, b).sortedWith(COMM).map(::lazyOf))
+            else -> C.Value.Def("int/add", listOf(a, b).sortedWith(COMM).map(::lazyOf))
         }
     },
     "int/sub" to {
@@ -35,7 +35,7 @@ val BUILTINS: Map<String, Normalizer.() -> C.Value> = mapOf(
             b is C.Value.IntOf && b.value == 0 -> a
             // a - a = 0
             a is C.Value.Var && b is C.Value.Var && a.level == b.level -> C.Value.IntOf(0)
-            else -> C.Value.Def("int/sub", mutableListOf(a, b).map(::lazyOf))
+            else -> C.Value.Def("int/sub", listOf(a, b).map(::lazyOf))
         }
     },
     "int/mul" to {
@@ -51,7 +51,7 @@ val BUILTINS: Map<String, Normalizer.() -> C.Value> = mapOf(
             a is C.Value.IntOf && a.value == 1 -> b
             // a * 1 = a
             b is C.Value.IntOf && b.value == 1 -> a
-            else -> C.Value.Def("int/mul", mutableListOf(a, b).sortedWith(COMM).map(::lazyOf))
+            else -> C.Value.Def("int/mul", listOf(a, b).sortedWith(COMM).map(::lazyOf))
         }
     },
     "int/div" to {
@@ -63,7 +63,7 @@ val BUILTINS: Map<String, Normalizer.() -> C.Value> = mapOf(
             b is C.Value.IntOf && b.value == 1 -> a
             // a / a = 1
             a is C.Value.Var && b is C.Value.Var && a.level == b.level -> C.Value.IntOf(1)
-            else -> C.Value.Def("int/div", mutableListOf(a, b).map(::lazyOf))
+            else -> C.Value.Def("int/div", listOf(a, b).map(::lazyOf))
         }
     },
     "int/mod" to {
@@ -75,13 +75,35 @@ val BUILTINS: Map<String, Normalizer.() -> C.Value> = mapOf(
             b is C.Value.IntOf && b.value == 1 -> a
             // a % a = 0
             a is C.Value.Var && b is C.Value.Var && a.level == b.level -> C.Value.IntOf(0)
-            else -> C.Value.Def("int/mod", mutableListOf(a, b).map(::lazyOf))
+            else -> C.Value.Def("int/mod", listOf(a, b).map(::lazyOf))
+        }
+    },
+    "byte_array/length" to {
+        when (val bs = lookup(size - 1)) {
+            is C.Value.ListOf -> C.Value.IntOf(bs.elements.size)
+            else -> C.Value.Def("byte_array/length", listOf(bs).map(::lazyOf))
+        }
+    },
+    "int_array/length" to {
+        when (val `is` = lookup(size - 1)) {
+            is C.Value.ListOf -> C.Value.IntOf(`is`.elements.size)
+            else -> C.Value.Def("int_array/length", listOf(`is`).map(::lazyOf))
+        }
+    },
+    "long_array/length" to {
+        when (val ls = lookup(size - 1)) {
+            is C.Value.ListOf -> C.Value.IntOf(ls.elements.size)
+            else -> C.Value.Def("long_array/length", listOf(ls).map(::lazyOf))
         }
     },
     "list/length" to {
         when (val `as` = lookup(size - 1)) {
             is C.Value.ListOf -> C.Value.IntOf(`as`.elements.size)
-            else -> C.Value.Def("list/length", listOf())
+            else -> {
+                val α = lookup(size - 3)
+                val n = lookup(size - 2)
+                C.Value.Def("list/length", listOf(α, n, `as`).map(::lazyOf))
+            }
         }
     }
 )
