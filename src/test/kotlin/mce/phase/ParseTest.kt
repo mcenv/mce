7647a62256b25fcc1e5b2a22c212c24a1b3a69f3
intsuc <intsuc@gmail.com>
2022-03-10 17:49:49 +0900

    Remove unnecessary test

--- src/test/kotlin/mce/phase/ParseTest.kt
+++ src/test/kotlin/mce/phase/ParseTest.kt
@@ -10,11 +10,6 @@ class ParseTest {
     private fun parse(name: String): S.Item = fetch(Key.SurfaceItem(name))
 
     @Test
-    fun parse() {
-        parse("parse")
-    }
-
-    @Test
     fun infixNonAssociative() {
         assertFails {
             parse("infix_non_associative")
