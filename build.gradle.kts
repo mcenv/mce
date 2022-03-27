cc15b0b6742ab9e3ad774c1503f9247a2e0af088
intsuc <intsuc@gmail.com>
2022-03-28 06:12:10 +0900

    Move to multiplatform project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import kotlinx.benchmark.gradle.JvmBenchmarkTarget
 
 plugins {
-    kotlin("jvm") version "1.6.10"
+    kotlin("multiplatform") version "1.6.10"
     kotlin("plugin.allopen") version "1.6.10"
     kotlin("plugin.serialization") version "1.6.10"
     id("org.jetbrains.kotlinx.benchmark") version "0.4.2"
@@ -14,18 +14,49 @@ repositories {
     mavenCentral()
 }
 
-dependencies {
-    implementation(kotlin("stdlib"))
-    implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.5")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.3.2")
-    testImplementation(kotlin("test"))
-    testImplementation("org.jetbrains.kotlinx:kotlinx-benchmark-runtime:0.4.2")
-}
+kotlin {
+    jvm {
+        testRuns["test"].executionTask.configure {
+            useJUnitPlatform()
+        }
+    }
 
-tasks.test {
-    useJUnitPlatform()
+    js {
+        nodejs()
+    }
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(kotlin("stdlib"))
+                implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.5")
+                implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
+                implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3.4")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-core:1.3.2")
+            }
+        }
+
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test"))
+            }
+        }
+
+        val jvmMain by getting {
+        }
+
+        val jvmTest by getting {
+            dependencies {
+                implementation("org.jetbrains.kotlinx:kotlinx-benchmark-runtime:0.4.2")
+            }
+        }
+
+        val jsMain by getting {
+        }
+
+        val jsTest by getting {
+        }
+    }
 }
 
 application {
@@ -38,7 +69,7 @@ allOpen {
 
 benchmark {
     targets {
-        register("test") {
+        register("jvmTest") {
             if (this is JvmBenchmarkTarget) {
                 jmhVersion = "1.34"
             }
