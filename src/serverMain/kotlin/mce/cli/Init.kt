114ecdaa8b4a52726cdfe9974c04e9a3ad529da2
intsuc <intsuc@gmail.com>
2022-04-07 10:38:47 +0900

    Generate `pom.xml` on `init`

--- src/serverMain/kotlin/mce/cli/Init.kt
+++ src/serverMain/kotlin/mce/cli/Init.kt
@@ -4,11 +4,19 @@ import kotlinx.cli.ExperimentalCli
 import kotlinx.cli.Subcommand
 import java.nio.file.Files
 import java.nio.file.Paths
+import kotlin.io.path.createFile
+import kotlin.io.path.writeLines
 
 @OptIn(ExperimentalCli::class)
 object Init : Subcommand("init", "Initialize project") {
     override fun execute() {
-        Files.createFile(Paths.get("pack.mce")) // TODO: add content
+        Paths.get("pack.mce").createFile() // TODO: add content
+        Paths.get("pom.xml").writeLines(
+            listOf(
+                "<dependencies>",
+                "</dependencies>",
+            )
+        )
         Files.createDirectory(Paths.get("src"))
     }
 }
