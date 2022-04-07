680345baa723fcf7dc704c89f09e5fc44f6d2e9e
intsuc <intsuc@gmail.com>
2022-04-07 20:51:04 +0900

    Close http client

--- src/clientMain/kotlin/mce/editor/Main.kt
+++ src/clientMain/kotlin/mce/editor/Main.kt
@@ -4,6 +4,7 @@ import io.ktor.client.*
 import io.ktor.client.engine.js.*
 import io.ktor.client.plugins.websocket.*
 import io.ktor.serialization.kotlinx.*
+import io.ktor.utils.io.core.*
 import kotlinx.coroutines.MainScope
 import kotlinx.coroutines.launch
 import kotlinx.serialization.ExperimentalSerializationApi
@@ -13,17 +14,18 @@ import mce.serialization.Mce
 @ExperimentalSerializationApi
 fun main() {
     MainScope().launch {
-        val client = HttpClient(Js) {
+        HttpClient(Js) {
             install(WebSockets) {
                 contentConverter = KotlinxWebsocketSerializationConverter(Mce)
             }
-        }
-        client.webSocket(host = "localhost", port = 51130) {
-            electron.onOpenFile { _, path -> println(path) }
+        }.use { client ->
+            client.webSocket(host = "localhost", port = 51130) {
+                electron.onOpenFile { _, path -> println(path) }
 
-            while (true) {
-                val response = receiveDeserialized<Response>()
-                println(response) // TODO
+                while (true) {
+                    val response = receiveDeserialized<Response>()
+                    println(response) // TODO
+                }
             }
         }
     }
