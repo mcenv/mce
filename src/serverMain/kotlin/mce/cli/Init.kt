b811d31f3031394efd5daeba153f8b20fb4f09ed
intsuc <intsuc@gmail.com>
2022-04-18 20:21:04 +0900

    Don't generate pom.xml

--- src/serverMain/kotlin/mce/cli/Init.kt
+++ src/serverMain/kotlin/mce/cli/Init.kt
@@ -5,18 +5,11 @@ import kotlinx.cli.Subcommand
 import java.nio.file.Files
 import java.nio.file.Paths
 import kotlin.io.path.createFile
-import kotlin.io.path.writeLines
 
 @ExperimentalCli
 object Init : Subcommand("init", "Initialize project") {
     override fun execute() {
         Paths.get("pack.mce").createFile() // TODO: add content
-        Paths.get("pom.xml").writeLines(
-            listOf(
-                "<dependencies>",
-                "</dependencies>",
-            )
-        )
         Files.createDirectory(Paths.get("src"))
     }
 }
