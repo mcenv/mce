94cf25c2a188ec435692027eb15932f1c32906d6
intsuc <intsuc@gmail.com>
2022-04-25 00:15:03 +0900

    Fix dependency conflict

--- build.gradle.kts
+++ build.gradle.kts
@@ -53,9 +53,9 @@ kotlin {
 
         val serverMain by getting {
             dependencies {
-                implementation("org.jetbrains.kotlinx:kotlinx-cli:0.3.4")
-                implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable:0.3.5")
-                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.3.2")
+                implementation("org.jetbrains.kotlinx:kotlinx-cli-jvm:0.3.4")
+                implementation("org.jetbrains.kotlinx:kotlinx-collections-immutable-jvm:0.3.5")
+                implementation("org.jetbrains.kotlinx:kotlinx-serialization-json-jvm:1.3.2")
                 implementation("io.ktor:ktor-server-core:2.0.0-eap-363")
                 implementation("io.ktor:ktor-server-netty:2.0.0-eap-363")
                 implementation("io.ktor:ktor-server-websockets:2.0.0-eap-363")
