9584b1ee89ee82a52127953eed7302df580ae90f
intsuc <intsuc@gmail.com>
2022-06-16 18:03:30 +0900

    Remove electron

--- environment/app/preload.js
+++
@@ -1,8 +0,0 @@
-const { contextBridge, ipcRenderer } = require("electron")
-
-contextBridge.exposeInMainWorld("electron", {
-    onNewPack: callback => ipcRenderer.on("new-pack", callback),
-    onExit: callback => ipcRenderer.on("exit", callback)
-})
-
-window.addEventListener("DOMContentLoaded", () => { /* TODO */ })
